dd24fa61a7d67081929241e69984215735cb5c7f
KRONIK666 <kronik@abv.bg>
2015-07-31 23:33:00 +0300

    Classwork

--- Classwork/ClassworkArrays/Task5/Program.cs
+++ Classwork/ClassworkArrays/Task5/Program.cs
@@ -12,35 +12,45 @@ namespace Task5
         {
             Console.Write("Enter sum to compare with possible sums: ");
             int s = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
             Console.Write("How many numbers to enter: ");
             int n = int.Parse(Console.ReadLine());
-            int[] myArray = new int[n];
-            int sum = 0;
             Console.WriteLine();
+
+            int[] myArray = new int[n];
+            bool[] checkSum = new bool[s + 1];
+            int checker = int.MinValue;
 
             for (int i = 0; i < myArray.Length; i++)
             {
                 Console.Write("Enter next element: ");
                 myArray[i] = int.Parse(Console.ReadLine());
             }
+
+            Console.WriteLine();
             Console.WriteLine("Your numbers are:");
+            Console.WriteLine();
+
             foreach (var item in myArray)
             {
                 Console.WriteLine(item);
             }
-            for (int i = 0; i < myArray.Length; i++)
+
+            foreach (var item in myArray)
             {
-                sum = myArray[i] + sum;
-                if (sum == s)
+                if (item > s)
                 {
-                    Console.WriteLine("Yes!");
                     continue;
                 }
-                else
+                for (int i = 0; i < checkSum.Length; i++)
                 {
-                    Console.WriteLine("No!");
-                    break;
-                }
+                    if (checkSum[i] == true && checker != 1)
+                    {
+                        checkSum[i + item] = true;
+                        checker = i + item;
+                    }
+                } checkSum[item] = true;
             }
         }
     }
---
+++ Classwork/ClassworkMethodsAndRecursion/Methods/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array's length: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] myArray = new int[n];
+            Console.WriteLine();
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write("Enter element: ");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Your array backwards is:");
+            Console.WriteLine();
+
+            PrintArray(myArray);
+
+            Console.WriteLine();
+        }
+
+        private static void PrintArray(int[] myArray)
+        {
+            for (int i = myArray.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+
+        Console.Write("Enter length: ");
+            int n = int.Parse(Console.ReadLine());
+            string[] massive = new string[n];
+
+            for (int i = 0; i < massive.Length; i++)
+            {
+                Console.Write("Enter element: ");
+                massive[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Your array is:");
+            Console.WriteLine();
+
+            BuildArray(massive);
+
+            Console.WriteLine();
+        }
+
+        private static void BuildArray(string[] massive)
+        {
+            for (int i = massive.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(massive[i]);
+            }
+    }
+}
