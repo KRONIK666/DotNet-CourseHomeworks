ffc0f4834d34be33e18fdfb5a9d244f9f27cda5d
KRONIK666 <kronik@abv.bg>
2015-08-02 14:12:23 +0300

    Homework

---
+++ Homeworks/HomeworkArraysAndMethods/Methods6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("{0}", n);
+            Console.WriteLine("! = " + Factorial(n));
+            Console.WriteLine();
+        }
+
+        private static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+    }
+}
