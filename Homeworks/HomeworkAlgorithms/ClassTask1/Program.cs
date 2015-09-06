dd1e42048361e45559f60c77b8bdf60b8d4d2cfe
KRONIK666 <kronik@abv.bg>
2015-09-06 03:13:06 +0300

    Homeworks

---
+++ Homeworks/HomeworkAlgorithms/ClassTask1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write("Enter number to fill the array: ");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter a number to check if it exists in the array: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int temp = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                for (int j = 0; j < myArray.Length; j++)
+                {
+                    if (myArray[i] < myArray[j])
+                    {
+                        temp = myArray[i];
+                        myArray[i] = myArray[j];
+                        myArray[j] = temp;
+                    }
+                }
+            }
+
+            int index = Array.BinarySearch(myArray, n);
+
+            if (index == -1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("This number doesn't exist in the array!");
+                Console.WriteLine();
+            }
+            else
+            {
+                while (n == myArray[index])
+                {
+                    index++;
+                }
+                Console.WriteLine();
+                Console.WriteLine("The next bigger number in the array is: " + myArray[index]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homeworks/HomeworkAlgorithms/ClassTask3/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 250;
+            int max = 0;
+            int[] myArray = new int[n];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                myArray[i] = rand.Next(0, 1000000);
+            }
+
+            int maxElement = Environment.TickCount;
+            Console.WriteLine("Max element is: " + FindMaxElement(myArray, max));
+            Console.WriteLine("Finding max element time in miliseconds: " + (Environment.TickCount - maxElement));
+            Console.WriteLine();
+
+            int quickSort = Environment.TickCount;
+            QuicksortArray(myArray, 0, n - 1);
+            Console.WriteLine("Quicksorting time in miliseconds: " + (Environment.TickCount - quickSort));
+            Console.WriteLine();
+
+            int print = Environment.TickCount;
+            PrintArray(myArray);
+            Console.WriteLine("Printing time in miliseconds: " + (Environment.TickCount - print));
+            Console.WriteLine();
+        }
+
+        static int FindMaxElement(int[] array, int max)
+        {
+            max = int.MinValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            } return max;
+        }
+
+        static void QuicksortArray(int[] array, int left, int right)
+        {
+            int temp = 0;
+            int i = left, j = right;
+            int pivot = array[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (array[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (array[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+
+                    i++;
+                    j--;
+                }
+
+                if (left < j)
+                {
+                    QuicksortArray(array, left, j);
+                }
+
+                if (i < right)
+                {
+                    QuicksortArray(array, i, right);
+                }
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine("The array has been sorted as follows:");
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + " ");
+            } Console.WriteLine();
+        }
+    }
+}
